26815b941c4f2f2d7802a52f79ed4ac647a8281a
Akashanil-2000 <chazeakash@gmail.com>
2024-05-02 10:31:08 +0530

    Completed

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 service = Service(executable_path="chromedriver.exe")
@@ -9,9 +11,21 @@ driver = webdriver.Chrome(service=service)
 
 driver.get("https://google.com")
 
-input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+WebDriverWait(driver,5).until(
+    EC.presence_of_all_elements_located((By.CLASS_NAME, "gLFyf"))
+)
 
-input_element.send_keys("Geeks for geeks" + Keys.ENTER)
+input_element = driver.find_element(By.CLASS_NAME, "gLFyf")
+input_element.clear()
+input_element.send_keys("GeeksforGeeks"+Keys.ENTER)
+
+WebDriverWait(driver,5).until(
+    EC.presence_of_all_elements_located((By.PARTIAL_LINK_TEXT,"GeeksforGeeks"))
+)
+
+
+link = driver.find_element(By.PARTIAL_LINK_TEXT,"GeeksforGeeks")
+link.click()
 
 time.sleep(10)
 
